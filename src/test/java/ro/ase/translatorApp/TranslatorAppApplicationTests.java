570ec06d91a12f186d56fff2537e736679e2342e
Corina Strachinariu <corina-andreea.strachinariu@sap.com>
2019-12-12 18:17:40 +0200

    init setup

---
+++ src/test/java/ro/ase/translatorApp/TranslatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ase.translatorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
