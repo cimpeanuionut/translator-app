38dc4ddd634360a48d69ee4faf283e2eb4b82921
Kethanol <denis.alexandru97@gmail.com>
2020-01-04 18:14:25 +0200

    Translate Endpoint + tests

--- src/test/java/ro/ase/translatorApp/TranslatorAppApplicationTests.java
+++ src/test/java/ro/ase/translatorApp/TranslatorAppApplicationTests.java
@@ -1,13 +1,36 @@
 package ro.ase.translatorApp;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ro.ase.translatorApp.controllers.TranslateController.TranslateController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest
 class TranslatorAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = standaloneSetup(new TranslateController()).build();
 	}
+
+	@Test
+	void testController() throws Exception {
+		this.mockMvc.perform(get("/api/translate"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("text/plain;charset=ISO-8859-1"));
+	}
+
+
 
 }
